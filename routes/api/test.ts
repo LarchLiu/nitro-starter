0e9fa9449e353d28dc58dc246069a5f280f145eb
Alex.Liu <larch.liu@gmail.com>
2023-05-23 15:46:17 +0800

    chore: test

--- routes/api/test.ts
+++ routes/api/test.ts
@@ -1,9 +1,6 @@
 import { eventHandler } from 'h3'
 
 export default eventHandler((event) => {
-  // const req = await readBody<{ webUrl: string }>(event)
-  // const webUrl = req.webUrl
-
   return {
     test: 'ok',
   }
