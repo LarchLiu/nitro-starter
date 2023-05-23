676a646f84dbfd64ed6d33ce49e884b8cb44dd1a
Alex.Liu <larch.liu@gmail.com>
2023-05-23 22:31:57 +0800

    chore: test

--- routes/api/webcard.ts
+++ routes/api/webcard.ts
@@ -2,9 +2,7 @@
 
 import { errorMessage } from '@starnexus/core/utils'
 import { SupabaseImageStorage } from '@starnexus/core/storage/supabase'
-import { NotionDataStorage } from '@starnexus/core/storage/notion'
-import type { IDataStorage, IImageStorage } from '@starnexus/core/storage'
-import type { WebInfoData } from '@starnexus/core'
+import type { IImageStorage } from '@starnexus/core/storage'
 
 export default eventHandler(async (event) => {
   try {
@@ -18,16 +16,16 @@ export default eventHandler(async (event) => {
     } = await readBody(event)
     // let imgRes: SavedImage
     const imgStorage = new SupabaseImageStorage(imgCfg) as IImageStorage
-    const dataStorage = new NotionDataStorage(dataCfg) as IDataStorage
-    const res = await createWebCard(webData as WebInfoData)
-    // const imgQuery = await imgStorage.query(res.imgPath)
-    // if (imgQuery.url)
-    //   imgRes = await imgStorage.update(res)
-    // else
-    const imgRes = await imgStorage.create(res)
+    // const dataStorage = new NotionDataStorage(dataCfg) as IDataStorage
+    // const res = await createWebCard(webData as WebInfoData)
+    // // const imgQuery = await imgStorage.query(res.imgPath)
+    // // if (imgQuery.url)
+    // //   imgRes = await imgStorage.update(res)
+    // // else
+    // const imgRes = await imgStorage.create(res)
 
-    const updateRes = await dataStorage.updateOgImage(savedData, imgRes.url)
-    return res
+    // const updateRes = await dataStorage.updateOgImage(savedData, imgRes.url)
+    return imgCfg
   }
   catch (error: any) {
     setResponseStatus(event, 400)
