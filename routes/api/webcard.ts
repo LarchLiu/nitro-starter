060f8721f44f0c4ee10de096eb6304a05f532383
Alex.Liu <larch.liu@gmail.com>
2023-05-23 23:59:09 +0800

    chore: test

--- routes/api/webcard.ts
+++ routes/api/webcard.ts
@@ -1,6 +1,7 @@
 // server/api/webcard.ts
 
 import { errorMessage } from '@starnexus/core/utils'
+import { SupabaseImageStorage } from '@starnexus/core/storage/supabase'
 
 export default eventHandler(async (event) => {
   try {
@@ -13,7 +14,7 @@ export default eventHandler(async (event) => {
       savedData,
     } = await readBody(event)
     // let imgRes: SavedImage
-    // const imgStorage = new SupabaseImageStorage(imgCfg) as IImageStorage
+    const imgStorage = new SupabaseImageStorage(imgCfg)
     // const dataStorage = new NotionDataStorage(dataCfg) as IDataStorage
     // const res = await createWebCard(webData as WebInfoData)
     // // const imgQuery = await imgStorage.query(res.imgPath)
